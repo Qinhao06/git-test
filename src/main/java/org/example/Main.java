55940880f42ec9028c7b58c8a0c84afba2cabd7e
Qinhao06 <935877636@qq.com>
2024-09-20 11:35:39 +0800

    123

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!12222232323");
+        System.out.println("Hello world!1222223232323");
     }
 }
