9cabb81cb0fb223a38d1796d8e0cccadd9326cf4
Qinhao06 <935877636@qq.com>
2024-12-01 19:23:22 +0800

    Revert "123111"
    This reverts commit 3e49bf00

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello 222232!");
+        System.out.println("Hello world!1222662236223");
     }
 }
