97adc20e535749e5a60d8450686ee839738b83c4
someone <935877636@qq.com>
2024-12-11 15:12:03 +0800

    mian-1-2
    (cherry picked from commit 2e5a3d2fea52ec17349ad86753d5dd4c34c9bac5)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println();
         System.out.println(1);
         System.out.println(1);
-        System.out.println();
+        System.out.println(1);
         System.out.println();
         System.out.println("Hello world!");
         System.out.println();
