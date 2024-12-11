f81ef15cfe7ce46aeb16efda7528b12e2eeadcfa
someone <935877636@qq.com>
2024-12-11 15:13:16 +0800

    mian-1-2
    (cherry picked from commit 2e5a3d2fea52ec17349ad86753d5dd4c34c9bac5)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
         System.out.println(1);
         System.out.println(1);
         System.out.println(1);
-        System.out.println();
+        System.out.println(1);
+        System.out.println(1);
         System.out.println("Hello world!");
         System.out.println();
     }
