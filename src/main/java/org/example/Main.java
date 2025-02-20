77d915a16981dc1315037934296f039b073603f5
someone <72430266+Qinhao06@users.noreply.github.com>
2025-02-20 16:16:47 +0800

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-      System.out.println("Hello world!1222662232322131111236223");
+      System.out.println("Hello world!122266223232213111121136223");
     }
 }
